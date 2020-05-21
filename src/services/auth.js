fce7c6f1b4fc09146f0d2cad18116a177a6cde67
jonataspinto <jonatastdb_jp@hotmail.com>
2020-05-21 14:18:42 -0300

    fix: eslint errors

--- src/services/auth.js
+++ src/services/auth.js
@@ -15,47 +15,51 @@ import { firebase } from './Firebase';
 //     });
 // }
 
-export const userAuthenticated = ()=>{
-    const user = localStorage.getItem("user")
-    if(user) {
-        return true
-    }else{
-        return false
-    }
-}
+export const userAuthenticated = () => {
+  const user = localStorage.getItem('user');
+  if (user) {
+    return true;
+  }
+  return false;
+};
 
-export const LoginFacebook = ()=>{
-    let provider = new firebase.auth.FacebookAuthProvider();
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then((result) =>{
-        localStorage.setItem("displayName", result.user.displayName)
-        localStorage.setItem("photoURL", result.user.photoURL)
-        localStorage.setItem("user", result.user)
-        console.log(result.user);
-        document.location.reload(true);
-        return true
-      })
-      .catch(function(error) {
-        // // Handle Errors here.
-        // var errorCode = error.code;
-        // var errorMessage = error.message;
-        // // The email of the user's account used.
-        // var email = error.email;
-        // // The firebase.auth.AuthCredential type that was used.
-        // var credential = error.credential;
-        // // ...
+export const LoginFacebook = () => {
+  const provider = new firebase.auth.FacebookAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(result => {
+      localStorage.setItem('displayName', result.user.displayName);
+      localStorage.setItem('photoURL', result.user.photoURL);
+      localStorage.setItem('user', result.user);
+      console.log(result.user);
+      document.location.reload(true);
+      return true;
+    })
+    .catch(error => {
+      // // Handle Errors here.
+      // var errorCode = error.code;
+      // var errorMessage = error.message;
+      // // The email of the user's account used.
+      // var email = error.email;
+      // // The firebase.auth.AuthCredential type that was used.
+      // var credential = error.credential;
+      // // ...
+      return error;
     });
-}
+};
 
-export const logOutFacebook =()=>{
-    firebase.auth().signOut().then(function(result) {
-        localStorage.removeItem("displayName")
-        localStorage.removeItem("photoURL")
-        localStorage.removeItem("user")
-        console.log("logout successful")
-    }).catch(function(error) {
-        console.log(error)
+export const logOutFacebook = () => {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      localStorage.removeItem('displayName');
+      localStorage.removeItem('photoURL');
+      localStorage.removeItem('user');
+      console.log('logout successful');
+    })
+    .catch(error => {
+      console.log(error);
     });
-}
+};
